9863a6bedbba5203f66526cb93169e6213bb3994
jeremyclayton <jeremy.clayton.91@gmail.com>
2017-04-24 16:50:09 -0600

    added movie component to da thing

--- AXIOS.md
+++ AXIOS.md
@@ -1,3 +1,19 @@
 #Axios and starting to CRUD in VueJs
 
-- We can install this method to 
+- We can install this method to
+```
+npm install -S axios
+```
+- in main js
+```
+import axios from 'axios'
+Vue.use(axios)
+```
+- also import into script tag
+- Create a input and submit button
+- Data bind the input with V - model
+- Create a data object that return an empty string called movies
+- Create a methods objects
+- Add event handler to submit button
+- Do an axios request to OMDB Api, use template literals at end with movie property
+- Add v-for
--- setupguide/src/components/movies.vue
+++ setupguide/src/components/movies.vue
@@ -3,5 +3,33 @@
   <div id="app">
       <h1>Movie Page</h1>
     <router-link to="home"><p>Home</p></router-link>
+
+  <div class="main">
+      <input type="text" name="" value="" v-model="movie">
+      <button type="button" class="btn btn-default" v-on:click="submit(movie)">Submit</button>
+        <div v-for="movie in movieResult">
+            <img :src="movie.Poster" alt="">
+            <h2>{{movie.Title}}</h2>
+        </div>
   </div>
+   </div>
 </template>
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            movie: '',
+            movieResult: []
+        }
+    },
+    methods: {
+        submit: function(movies) {
+            axios.get(`http://www.omdbapi.com/?s=${this.movie}`).then(response => {
+                console.log(response.data);
+                this.movieResult = response.data.Search
+            })
+        }
+    }
+}
+</script>
